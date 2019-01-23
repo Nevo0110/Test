8c8a86fc873fef9ae78aea74f5907b62ddd91c18
Nevo0110 <ngordon@westshore.edu>
2019-01-23 14:31:54 -0500

    Fixed bug and testing pull

--- TestCommit.c
+++ TestCommit.c
@@ -2,6 +2,7 @@
 
 int main(){
   printf("This is a test commit\n");
-  printf("This is a commit from VS17\n")
+  printf("This is a commit from VS17\n"); //also fixed this error, (no ';')
+  printf("This is a test, trying to do a pull in VS17\n");
   return 0;
 }
